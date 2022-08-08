edf28fd8bed99ada3feea868647f423b8eb63f6f
matondo antonio <matondo.antonio@ideiasdinamicas.com>
2022-08-08 22:47:03 +0100

    add new feacture

--- index.js
+++ index.js
@@ -1,133 +1,154 @@
- import { jsPDF } from 'jspdf'
- import { calculateTheLengthOfText, percent, removePercent } from './helper'
+import * as jsPDF from 'jspdf';
+import { calculateTheLengthOfText, percent, removePercent } from './helper'
 
- const doc = new jsPDF()
+const docs = new jsPDF()
 
- let pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
- let pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
- let globalHeight = 0;
-
-function addPage(heigth, top) {
-  if (globalHeight >= heigth) {
-    doc.addPage();
-    globalHeight = top;
-  }
-}
+let pageHeight = docs.internal.pageSize.height || doc.internal.pageSize.getHeight();
+let pageWidth = docs.internal.pageSize.width || doc.internal.pageSize.getWidth();
+let globalHeight = 0;
 
 class Container {
 
-    constructor() {}
-  
-    div(doc, style) {
-      let border = style.border != undefined ? style.border : [255, 255, 255];
-      let bgColor = style.bgColor != undefined ? style.bgColor : [255, 255, 255];
-      let setColor = style.bgColor != undefined ? "FD" : undefined;
-      doc.setDrawColor(...border);
-      doc.setFillColor(...bgColor);
-      let height = percent(pageHeight, (globalHeight + removePercent(style.heigth)) + '%');
-      doc.rect(
-        percent(pageHeight, style.marginLeft),
-        height,
-        percent(pageWidth, style.width),
-        percent(pageWidth, style.padding),
-        setColor
-      );
-    }
+   constructor() {
+     this.doc = new jsPDF()
+   }
 
-    th(doc, style) {
-      let border = style.border != undefined ? style.border : [255, 255, 255];
-      let bgColor = style.bgColor != undefined ? style.bgColor : [255, 255, 255];
-      let setColor = style.bgColor != undefined ? "FD" : undefined;
-      doc.setDrawColor(...border);
-      doc.setFillColor(...bgColor);
-      let height = percent(pageHeight, (globalHeight + removePercent(style.heigth)) + '%');
-      doc.rect(
-        percent(pageHeight, style.marginLeft),
-        height,
-        percent(pageWidth, style.width),
-        percent(pageWidth, style.padding),
-        setColor
-      );
-    }
+   addPage(heigth, top) {
+     if (globalHeight >= heigth) {
+       this.doc.addPage();
+       globalHeight = top;
+     }
+   }
+ 
+   div(style) {
+     let border = style.border != undefined ? style.border : [255, 255, 255];
+     let bgColor = style.bgColor != undefined ? style.bgColor : [255, 255, 255];
+     let setColor = style.bgColor != undefined ? "FD" : undefined;
+     this.doc.setDrawColor(...border);
+     this.doc.setFillColor(...bgColor);
+     let height = percent(pageHeight, (globalHeight + removePercent(style.heigth)) + '%');
+     this.doc.rect(
+       percent(pageHeight, style.marginLeft),
+       height,
+       percent(pageWidth, style.width),
+       percent(pageWidth, style.padding),
+       setColor
+     );
+   }
 
-    td(doc, style) {
-      let border = style.border != undefined ? style.border : [255, 255, 255];
-      let bgColor = style.bgColor != undefined ? style.bgColor : [255, 255, 255];
-      let setColor = style.bgColor != undefined ? "FD" : undefined;
-      doc.setDrawColor(...border);
-      doc.setFillColor(...bgColor);
-      let height = percent(pageHeight, (globalHeight + removePercent(style.heigth)) + '%');
-      doc.rect(
-        percent(pageHeight, style.marginLeft),
-        height,
-        percent(pageWidth, style.width),
-        percent(pageHeight, style.padding),
-        setColor
-      );
-    }
+   th(style) {
+     let border = style.border != undefined ? style.border : [255, 255, 255];
+     let bgColor = style.bgColor != undefined ? style.bgColor : [255, 255, 255];
+     let setColor = style.bgColor != undefined ? "FD" : undefined;
+     this.doc.setDrawColor(...border);
+     this.doc.setFillColor(...bgColor);
+     let height = percent(pageHeight, (globalHeight + removePercent(style.heigth)) + '%');
+     this.doc.rect(
+       percent(pageHeight, style.marginLeft),
+       height,
+       percent(pageWidth, style.width),
+       percent(pageWidth, style.padding),
+       setColor
+     );
+   }
 
-    splitText(text, size, width, font = "") {
-      let lines = doc
-        .setFont(font)
-        .setFontSize(size)
-        .splitTextToSize(text, width || undefined);
-      return lines;
-    }
+   td(style) {
+     let border = style.border != undefined ? style.border : [255, 255, 255];
+     let bgColor = style.bgColor != undefined ? style.bgColor : [255, 255, 255];
+     let setColor = style.bgColor != undefined ? "FD" : undefined;
+     this.doc.setDrawColor(...border);
+     this.doc.setFillColor(...bgColor);
+     let height = percent(pageHeight, (globalHeight + removePercent(style.heigth)) + '%');
+     this.doc.rect(
+       percent(pageHeight, style.marginLeft),
+       height,
+       percent(pageWidth, style.width),
+       percent(pageHeight, style.padding),
+       setColor
+     );
+   }
 
-    p(text, style, callBack = 0) {
-      let addPosition = callBack;
-      doc.setFont("", style.fontWeigth);
-      doc.setFontSize(style.fontSize);
-      let marginTopPercent = percent(pageHeight, (globalHeight + removePercent(style.marginTop)) + '%');
-      let formatedText = text//this.splitText(text, style.fontSize, style.width, "");
-      doc.text(
-        percent(pageWidth, style.marginLeft),
-        marginTopPercent,
-        formatedText
-      );
-      marginTopPercent += addPosition;
-    }
-  
-    hr(style) {
-      doc.setFont("", 'bold');
-      doc.setDrawColor(0, 0, 0)
-      doc.setLineWidth(style.width || 0.5);
-      doc.line(
-        percent(pageWidth, style.marginLeft),
-        percent(pageHeight, (globalHeight + removePercent(style.top)) + '%' ),
-        percent(pageWidth, style.marginRight),
-        percent(pageHeight, (globalHeight + removePercent(style.bottom)) + '%' )
-      );
-    }
-  
-    Image(src, style) {
-      doc.addImage(
-        src,
-        "JPEG",
-        percent(pageWidth, style.marginLeft),
-        percent(pageWidth, globalHeight + removePercent(style.marginTop) + '%'),
-        percent(pageWidth, style.width),
-        percent(pageWidth, style.height)
-      );
-    }
-    
-    save() {
-      doc.save();
-    }
+   splitText(text, size, width, font = "") {
+     let lines = this.doc
+       .setFont(font)
+       .setFontSize(size)
+       .splitTextToSize(text, width || undefined);
+     return lines;
+   }
 
-    print() {
-      doc.autoPrint();
-      doc.output("dataurlnewwindow");
-    }
-  }
+   p(text, style, callBack = 0) {
+     let addPosition = callBack;
+     this.doc.setFont("", style.fontWeigth);
+     this.doc.setFontSize(style.fontSize);
+     let marginTopPercent = percent(pageHeight, (globalHeight + removePercent(style.marginTop)) + '%');
+     let formatedText = this.validateTextField(text)
+     this.doc.text(
+       percent(pageWidth, style.marginLeft),
+       marginTopPercent,
+       formatedText,
+       { maxWidth: style.width ? style.width : 0, align: style.alignText || ""}
+     );
+     marginTopPercent += addPosition;
+   }
+ 
+   hr(style) {
+     this.doc.setFont("", 'bold');
+     this.doc.setDrawColor(0, 0, 0)
+     this.doc.setLineWidth(style.width || 0.5);
+     this.doc.line(
+       percent(pageWidth, style.marginLeft),
+       percent(pageHeight, (globalHeight + removePercent(style.top)) + '%' ),
+       percent(pageWidth, style.marginRight),
+       percent(pageHeight, (globalHeight + removePercent(style.bottom)) + '%' )
+     );
+   }
+ 
+   Image(src, style) {
+     this.doc.addImage(
+       src,
+       "JPEG",
+       percent(pageWidth, style.marginLeft),
+       percent(pageWidth, globalHeight + removePercent(style.marginTop) + '%'),
+       percent(pageWidth, style.width),
+       percent(pageWidth, style.height)
+     );
+   }
+   
 
-  export {
-    Container,
-    addPage,
-    calculateTheLengthOfText,
-    percent,
-    removePercent,
-    globalHeight,
-    doc
-  }
+   save(name = '', report = 'print') {
+     globalHeight = 0
+     if(report == 'print') {
+       this.doc.autoPrint();
+       window.open(this.doc.output("bloburl"));
+     }else {
+       this.doc.save(name);
+     }
+       
+   }
+   
+   validateTextField(text) {
+     if(text) {
+       return text
+     }
+     return '---'
+   }
+   setGlobalHeight(height) {
+     globalHeight = height
+   }
+   addUpGlobalHeight(height) {
+     globalHeight += height
+   }
+
+   reduceGlobalHeight(height) {
+     globalHeight -= height
+   }
+ }
+
+ export {
+   Container,
+   calculateTheLengthOfText,
+   percent,
+   removePercent,
+   globalHeight,
+ }
 
